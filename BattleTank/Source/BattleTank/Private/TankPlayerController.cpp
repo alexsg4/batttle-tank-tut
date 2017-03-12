b42daddac0329c81f448afdb4be32c15cf7dbcef
Alexandru Stanciu <stanciu.alex74@gmail.com>
2017-03-12 16:48:40 +0200

    Basic Logging for PlayerController

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -6,15 +6,21 @@
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+
+///Basic logging for player controller
 	UE_LOG(LogTemp, Warning, TEXT("Player Controller Begin Play!"));
+
+	if (GetControlledTank() == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("There is no posessed tank!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Posessed tank is: %s"), *GetControlledTank()->GetName());
+	}
 }
 
 ATank* ATankPlayerController::GetControlledTank() const 
 {
 	return Cast<ATank>(GetPawn());
 }
-
-//TODO Log posessed tank
-
-
-
