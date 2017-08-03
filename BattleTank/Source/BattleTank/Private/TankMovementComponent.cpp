5dc351c2089812a327729903c3927633a3166e7c
Alexandru Stanciu <stanciu.alex74@gmail.com>
2017-08-04 01:38:07 +0300

    Pathfinding: Tweak map and add Nav Mesh. Also guard correct pointers in MovementComponent

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -6,13 +6,13 @@
 
 void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
+	if (!LeftTrack || !RightTrack) { return; }
 	UE_LOG(LogTemp, Warning, TEXT("Intent move forward with throw: %f"), Throw);
 
 	LeftTrack->SetThrottle(Throw);
@@ -24,6 +24,7 @@ void UTankMovementComponent::IntendMoveForward(float Throw)
 
 void UTankMovementComponent::IntendTurnRight(float Throw)
 {
+	if (!LeftTrack || !RightTrack) { return; }
 	UE_LOG(LogTemp, Warning, TEXT("Intent move forward with throw: %f"), Throw);
 
 	LeftTrack->SetThrottle(Throw);
